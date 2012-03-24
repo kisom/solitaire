1823857a7f72a6ca0e8800187088463c707e58ae
Kyle Isom <coder@kyleisom.net>
2012-03-24 10:31:26 +0300

    Quick script to automate running all test suites.

---
+++ run_tests.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+echo "Starting tests..."
+echo "-----------------------------------------"
+test/card_test
+test/deck_test
+test/pontifex_test
+echo "-----------------------------------------"
+
