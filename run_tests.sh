864891f8f0efc52375410500dddaec10f4560e2b
Kyle Isom <coder@kyleisom.net>
2012-03-24 10:33:33 +0300

    set up test runner in `make check`

--- run_tests.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-echo "Starting tests..."
-echo "-----------------------------------------"
-test/card_test
-test/deck_test
-test/pontifex_test
-echo "-----------------------------------------"
-
